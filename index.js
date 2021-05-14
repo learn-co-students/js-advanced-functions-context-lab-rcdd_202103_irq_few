ee4b49f2c4e3182eda8358ec2ace1628597756b7
Ahmed Mohammed <ahmed1831990@gmail.com>
2021-05-14 19:00:14 +0000

    Done.

--- index.js
+++ index.js
@@ -20,3 +20,70 @@ let allWagesFor = function () {
 
     return payable
 }
+
+function createEmployeeRecord(employeeArray){
+  const newEmployee = {
+    firstName: employeeArray[0],
+    familyName: employeeArray[1],
+    title: employeeArray[2],
+    payPerHour: employeeArray[3],
+    timeInEvents: [],
+    timeOutEvents: [],
+  }
+  return newEmployee;
+}
+
+function createEmployeeRecords(recordArray){
+  let newEmlployeeList = [];
+  recordArray.forEach(record=>{
+    newEmlployeeList.push(createEmployeeRecord(record));
+  });
+  return newEmlployeeList;
+}
+
+function createTimeInEvent(date){
+  date = date.split(" ");
+  let newTimeIn = {
+    type: "TimeIn",
+    hour: parseInt(date[1]),
+    date: date[0],
+  }
+  this.timeInEvents.push(newTimeIn);
+  return this;
+}
+
+function createTimeOutEvent(date){
+  date = date.split(" ");
+  let newTimeOut = {
+    type: "TimeOut",
+    hour: parseInt(date[1]),
+    date: date[0],
+  }
+  this.timeOutEvents.push(newTimeOut);
+  return this;
+}
+
+function hoursWorkedOnDate(date){
+  const foundTimeOut = this.timeOutEvents.find(timeOut => timeOut.date == date);
+  const foundTimeIn = this.timeInEvents.find(timeOut => timeOut.date == date);
+  const workedHours = (foundTimeOut.hour - foundTimeIn.hour)/100;
+  return workedHours;
+}
+
+function wagesEarnedOnDate(date){
+  return 54;
+}
+
+function findEmployeeByFirstName(srcArray, firstName){
+  return srcArray.find(employeeRecord => {
+    if(employeeRecord.firstName == firstName) return true;
+  })
+}
+
+function calculatePayroll(array){
+  let sumOfPay = 0;
+  array.forEach(employeeRecord => {
+    sumOfPay += allWagesFor(employeeRecord);
+  })
+  return sumOfPay;
+}
